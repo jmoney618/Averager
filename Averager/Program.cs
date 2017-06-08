d2a629ecb5fd48db12836420bbd2d2efe6475542
Jose Quinones <jquinones618@gmail.com>
2017-06-07 22:54:43 -0400

    Implemented mechanism to quit

--- Averager/Program.cs
+++ Averager/Program.cs
@@ -11,8 +11,45 @@ namespace Averager
     {
         static void Main(string[] args)
         {
-            double[] inputs;
-            double average;
+            double[] numbers = { };
+
+            while (true)
+            {
+                try
+                {
+                    // Ask user to enter a number or quit
+                    Console.Write("Enter a number or type \'done\' to get the average: ");
+                    var input = Console.ReadLine();
+
+                    // Validate is user wants to quit
+                    if (input.ToLower() == "done" && numbers.Length == 0)
+                    {
+                        Console.WriteLine("You really didn\'t want to use this app did you?");
+                        Console.Write("Enter any key to quit.");
+                        return;
+                    }
+                    else
+                    {
+
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public double Average(double[] numbers)
+        {
+            double total = 0.0;
+            double average = 0.0;
+            foreach(double number in numbers)
+            {
+                total += number;
+            }
+
+            return average = total / numbers.Length;
         }
     }
 }
